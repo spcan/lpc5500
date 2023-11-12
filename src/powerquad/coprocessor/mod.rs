2a2722ada0d527aa83120fc7f6db4f849a74f6f7
agrc <agrc14@gmail.com>
2023-11-13 00:47:47 +0100

    feat: Improve PQ locks

--- src/powerquad/coprocessor/mod.rs
+++ src/powerquad/coprocessor/mod.rs
@@ -27,50 +27,51 @@ impl<const N: usize> Coprocessor<N> {
     }
 }
 
-impl<const N: usize> CoprocessorInterface<f32> for Coprocessor<N> {
+impl<'a, const N: usize> CoprocessorInterface<'a, f32> for Coprocessor<N> where Coprocessor<N>: 'a {
     const CPID: usize = N << 1;
 
-    type Result = Operation<N, f32>;
+    type Result = Operation<'a, N, f32>;
 
-    fn result(self, read: ReadType) -> Operation<N, f32> {
-        Operation::create( read )
+    fn result(&'a mut self, read: ReadType) -> Operation<'a, N, f32> {
+        Operation::create( read, self )
     }
 }
 
 
 
 /// An ongoing PowerQuad Coprocessor operation.
-pub struct Operation<const N: usize, NUMBER: CoprocessorNumber> {
+pub struct Operation<'a, const N: usize, NUMBER: CoprocessorNumber> {
     /// The read functino to use.
     read: ReadType,
+
+    /// This is a mutability lock on the origin coprocessor.
+    _lock: &'a Coprocessor<N>,
 
     _phantom: core::marker::PhantomData<NUMBER>,
 }
 
-impl<const N: usize, NUMBER: CoprocessorNumber> Operation<N, NUMBER> {
+impl<'a, const N: usize, NUMBER: CoprocessorNumber> Operation<'a, N, NUMBER> {
     /// Internal functino to create an ongoing operation.
-    pub(self) fn create( read: ReadType ) -> Self {
-        Self { read, _phantom: core::marker::PhantomData, }
+    pub(self) fn create( read: ReadType, _lock: &'a Coprocessor<N> ) -> Self {
+        Self { _lock, read, _phantom: core::marker::PhantomData, }
     }
 
     /// Internal read function of type MUL.
-    fn read<const READ: u32, const TYPE: usize>(self) -> (NUMBER, Coprocessor<N>) {
+    fn read<const READ: u32, const TYPE: usize>(self) -> NUMBER {
         // Read the result raw.
         let raw = asm::mrc::<{traits::PQID}, READ, TYPE, 0, 0>();
 
         // Transform into the type.
         let result = NUMBER::from( raw );
 
-        ( result, Coprocessor::create() )
+        result
     }
 }
 
-impl CoprocessorOperation<f32> for Operation<0, f32> {
+impl<'a> CoprocessorOperation<f32> for Operation<'a, 0, f32> {
     const CPID: usize = 0 << 1;
 
-    type Origin = Coprocessor<0>;
-
-    fn finish(self) -> (f32, Self::Origin) {
+    fn finish(self) -> f32 {
         match self.read {
             ReadType::Add => self.read::<1, {0 | (0 << 1)}>(),
             ReadType::Mul => self.read::<0, {0 | (0 << 1)}>(),
@@ -78,12 +79,10 @@ impl CoprocessorOperation<f32> for Operation<0, f32> {
     }
 }
 
-impl CoprocessorOperation<f32> for Operation<1, f32> {
+impl<'a> CoprocessorOperation<f32> for Operation<'a, 1, f32> {
     const CPID: usize = 1 << 1;
 
-    type Origin = Coprocessor<1>;
-
-    fn finish(self) -> (f32, Self::Origin) {
+    fn finish(self) -> f32 {
         match self.read {
             ReadType::Add => self.read::<1, {0 | (1 << 1)}>(),
             ReadType::Mul => self.read::<0, {0 | (1 << 1)}>(),
